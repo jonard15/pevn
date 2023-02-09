cf260365a414158732a4f71bee7a0ec1f8c2f0c5
jonard15 <lilkillah.111@gmail.com>
2023-02-09 09:34:31 +0800

    add vueroute

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,60 +1,57 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
+      <div>
+    <!-- <v-app-bar
+      color="deep-purple accent-4"
+      dense
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon>
+        <router-link to="/about">About</router-link>
       </v-btn>
-    </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+      <v-btn icon>
+       <router-link to="/">Home</router-link> |
+      </v-btn>
+
+    </v-app-bar> -->
+    <TopToolbar></TopToolbar>
+    <router-view/>
+    <BottomNav></BottomNav>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import TopToolbar from './components/TopToolbar.vue';
+import BottomNav from './components/BottomNav.vue';
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+    components: {
+    TopToolbar,
+    BottomNav,
   },
-
-  data: () => ({
-    //
-  }),
-};
+}
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-container>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Name
+            </th>
+            <th class="text-left">
+              Email
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="item in desserts"
+            :key="item.name"
+          >
+            <td>{{ item.name }}</td>
+            <td>{{ item.email }}</td>
+          </tr>
+        </tbody>
+      </template>
+  </v-simple-table>
+    </v-container>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+    data: () => ({
+      showPassword: false,
+      desserts: [],
+    }),
+
+    methods:{
+      async getUsers(){
+        const users = await fetch("http://localhost:5000/users")
+        const jsonData = await users.json()
+        this.desserts = jsonData
+      }
+    },
+
+    created () {
+      this.getUsers();
+    }
+}
+</script>
